86fbf3977df3d43cea5d97ddc544454d7f4abd7d
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-10 16:59:14 +0100

    js ok

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-
-
-document.getElementById('title').innerHTML = "HELLO WORLD";
-
-
-
 var nome = prompt ("Qual è il tuo nome?")
+console.log(nome);
+
 var cognome = prompt ("Qual è il tuo cognome?")
-var nome = prompt ("Qual è il tuo colore preferito")
+console.log(cognome);
+
+var colore = prompt ("Qual è il tuo colore preferito")
+console.log(colore);
+
+var pwd = nome + cognome + colore + "19";
+
+document.getElementById('nome').innerHTML = nome;
+document.getElementById('cognome').innerHTML = cognome;
+document.getElementById('colore').innerHTML = colore;
+document.getElementById('psw').innerHTML = pwd;
