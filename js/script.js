e5c82c2e84ddb2cd066d410251635884e530a529
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-10 17:37:53 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var nome = prompt ("Qual è il tuo nome?")
+var nome = prompt ("Qual è il tuo nome?");
 console.log(nome);
 
-var cognome = prompt ("Qual è il tuo cognome?")
+var cognome = prompt ("Qual è il tuo cognome?");
 console.log(cognome);
 
-var colore = prompt ("Qual è il tuo colore preferito")
+var colore = prompt ("Qual è il tuo colore preferito");
 console.log(colore);
 
 var pwd = nome + cognome + colore + "19";
