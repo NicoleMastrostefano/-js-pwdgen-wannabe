6bccb0cee519ddf9c05c794d09ccc26febe36782
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-11-10 16:12:49 +0100

    upload files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('title').innerHTML = "HELLO WORLD";
+
+
+
+var nome = prompt ("Qual è il tuo nome?")
+var cognome = prompt ("Qual è il tuo cognome?")
+var nome = prompt ("Qual è il tuo colore preferito")
